2c304cb1c61b08679647ed77c2b140b8cecd63c9
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2021-01-10 08:45:39 -0300

    Update js-api-req.js

--- js-api-req.js
+++ js-api-req.js
@@ -1,25 +1,27 @@
-  // https://www.javascripture.com
-  // generate req = POSTMAN -> code -> javascript
-  
-  // Parâmetros
-  let params = {
-    url: "https://reqres.in/api/users",
-  }
+// generate req = POSTMAN -> code -> javascript
+
+// Parâmetros
+let params = {
+  url: "https://reqres.in/api/users",
+}
 
 
-  // Requisição GET
-  var request = new Request(params.url);
-  fetch(request).then(function(response) {
-    return response.text();
-  }).then(function(response) {
-    response = JSON.parse(response)
-
-    // Mostra requisição no console
-    console.log(response)
+// --------------------------------  Requisição GET
+function get(){
+  let request = new Request(params.url, {
+    method: 'GET',
   });
 
-  // Requisição POST
-  var request = new Request(params.url, {
+  fetch(request).then(function(response) { return response.text() }).then(function(response){
+    response = JSON.parse(response)
+
+    console.log(response);
+  });
+}get()
+
+// --------------------------------  Requisição POST
+function post(){
+  let request = new Request(params.url, {
     method: 'POST',
     body: '{ "name": "Andressa", "job": "DevOps" }' 
   });
@@ -27,3 +29,4 @@
   request.json().then(function(response) { 
     console.log(`Nome: ${response.name}; Job: ${response.job}`);
   });
+}post()
